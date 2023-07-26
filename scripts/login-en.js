9b5699d5cb86ab7a7ebecbc9e7434254026354f2
Tim Xia <timxia@cmail.carleton.ca>
2023-07-26 16:14:04 -0400

    front now serves html with proxy, no logout and auth yet

--- scripts/login-en.js
+++ scripts/login-en.js
@@ -1,3 +1,40 @@
+// console.log("test 1");
+
+// let userRequestObj = {
+//     username: 'TimXia77',
+//     password: '1a2b3c4D'
+// };
+
+// let reload = false;
+
+// fetch('http://api:3000/login', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(userRequestObj),
+// })
+//     .then((res) => {
+//         console.log("test 2");
+//         if (res.ok) {
+//             return res.json();
+//         } else if (res.status == 400 || res.status == 409) {
+//             reload = true;
+//             return res.json();
+//         }
+//     })
+//     .then((data) => {
+//         if (reload) {
+//             res.redirect(`/api/register?msg=${data.error}`);
+//         } else {
+//             res.cookie("token", data.cookie);
+//             res.redirect("/api/table");
+//         }
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//     });
+
 
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('error');
--- scripts/table.js
+++ scripts/table.js
@@ -6,23 +6,26 @@ $(document).ready(function () {
     });
 });
 
+console.log("TEST");
 
 //Event Handlers
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('logoutButton').addEventListener('click', logout);
 })
 
-function logout(){ 
-    fetch(`/logout`, { 
+function logout() {
+    fetch('http://localhost:4000/logout', { //CHANGE to http://api:3000/logout later, when proxy is added to avoid cors 
         method: 'POST',
+        credentials: 'include', // Include cookies in the cross-origin request?
     })
     .then(response => {
-        if (response.ok) { //Logged out successfully 
-            window.location.href = response.url;      
-        } 
+        if (response.ok) { 
+            window.location.href = response.url;
+        }
     })
     .catch(error => {
         console.error('Error:', error);
     });
+
 }
 
